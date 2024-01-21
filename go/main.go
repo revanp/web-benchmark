434c363b795926ce545044a61bfae959e6ba1a15
revanp <revan@503-00341.modena.com>
2024-01-21 21:40:36 +0700

    First commit go and hyperf

---
+++ go/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var text string
+    for i := 0; i < 10000; i++ {
+        text += "Hello World! \n"
+    }
+    
+    fmt.Fprintf(w, text)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ hyperf/app/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+class IndexController extends AbstractController
+{
+    public function index()
+    {
+        $text = '';
+        for($i = 0; $i < 10000; $i++){
+            $text .= "Hello World! \n";
+        }
+
+        return $text;
+    }
+}
